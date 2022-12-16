f96aee2586e7a96801baf27e270736be32e32734
abdulhamid <rayca7882@gmail.com>
2022-12-16 05:01:49 -0800

    combination

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,15 +7,16 @@ int main(void)
 {
 	int n;
 
-	for (n = 48; n < 58; n++)
+	for (n = 0; n <= 9; n++)
 	{
-		putchar(n);
+		putchar((n % 10) + '0');
+		if (n == 9)
+			continue;
+
+		putchar(',');
+		putchar(' ');
 	}
-		if (n != 57)
-		{
-			putchar(',');
-			putchar(' ');
-		}
-		putchar('\n');
-		return (0);
+	putchar('\n');
+	return (0);
 }
+
